6bc9dcf82a8a2694dd8d0597ce4b7d03b6dcc7dc
Chris Kivaste <chris-kristjan.kivaste@carrentalgateway.com>
2024-11-03 19:54:12 +0200

    Add a welcome section to the main page

--- vue3-vite-ts-pinia-simple-project/src/App.vue
+++ vue3-vite-ts-pinia-simple-project/src/App.vue
@@ -2,10 +2,27 @@
   <div id="nav">
     <router-link to="/">Home</router-link>|
     <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
   </div>
   <img alt="Vue logo" src="@/assets/images/logo.png" width="120" />
+  <div class="welcome-message">
+    <h1>Welcome to the App</h1>
+    <p>This is a sample application.</p>
+    <img src="welcome-image.png" alt="Welcome Image" />
+    <button @click="showAlert">Click Me</button>
+  </div>
   <router-view />
 </template>
+
+<script lang="ts">
+export default {
+  methods: {
+    showAlert() {
+      alert("Welcome!");
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -24,6 +41,23 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+.welcome-message {
+  margin: 20px;
+  border: 1px solid #ccc;
+  padding: 15px;
+  h1 {
+    color: red;
+    font-size: 2em;
+  }
+  p {
+    font-size: 1.2em;
+    color: blue;
+  }
+  img {
+    width: 100px;
+    height: auto;
   }
 }
 </style>
